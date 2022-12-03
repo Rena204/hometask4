c9dbb63ddb4bc183cb75b58e1084d3f6f37fbd56
rena <rena2004.rr@gmail.com>
2022-12-03 15:10:48 +0400

    hometask4

---
+++ task_25/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите число А :");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B :");
+int B = Convert.ToInt32(Console.ReadLine());
+int start = A;
+for (int i = 1; i<B; i++) {
+    start = start * A;
+}
+Console.Write("A в степени В = " + start);
---
+++ task_29/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Clear();
+int [] array = new int[8] {1, 2, 5, 7, 19, 6, 1, 33};
+for (int i =0 ; i<1 ;i++) {
+    Console.Write($"Результат: [{string.Join(",", array)}]");
+}
